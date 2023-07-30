6f4729369063a4ad6dbba3988ad1390085be214a
Navi212 <josephchristo615@gmail.com>
2023-07-30 16:00:02 +0100

    Adds Flush provileges

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -13,9 +13,9 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
 GRANT ALL PRIVILEGES
     ON hbnb_test_db.*
     TO 'hbnb_test'@'localhost'
-    WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
 
 GRANT SELECT
     ON performance_schema.*
     TO 'hbnb_test'@'localhost'
-    WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
