6d0e30832caf2f6fef454695bac8d5b34368b777
Navi212 <josephchristo615@gmail.com>
2023-08-01 23:43:33 +0100

    Adds vetted FS and DB strorage tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,85 +1,222 @@
 #!/usr/bin/python3
-"""test for file storage"""
-import unittest
-import pep8
-import json
-import os
-from os import getenv
+"""A test module for `DBStorage` class"""
+
+
 import MySQLdb
+import pep8
+import unittest
+from os import getenv
 from models.base_model import BaseModel, Base
-from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
-from models.place import Place
 from models.review import Review
+from models.city import City
+from models.state import State
+from models.user import User
+from models.place import Place
 from models.engine.db_storage import DBStorage
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 
 
-@unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-class TestDBStorage(unittest.TestCase):
-    '''this will test the DBStorage'''
-
-    @classmethod
-    def setUpClass(self):
-        """set up for test"""
-        self.User = getenv("HBNB_MYSQL_USER")
-        self.Passwd = getenv("HBNB_MYSQL_PWD")
-        self.Db = getenv("HBNB_MYSQL_DB")
-        self.Host = getenv("HBNB_MYSQL_HOST")
-        self.db = MySQLdb.connect(host=self.Host, user=self.User,
-                                  passwd=self.Passwd, db=self.Db,
-                                  charset="utf8")
-        self.query = self.db.cursor()
-        self.storage = DBStorage()
-        self.storage.reload()
+@unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+class Test_db_storage(unittest.TestCase):
+    """
+    Defines a `Test_db_storage` test class
+    that tests class attributes and methods
+    """
 
     @classmethod
-    def teardown(self):
-        """at the end of the test this will tear it down"""
-        self.query.close()
-        self.db.close()
+    def setUpClass(cls):
+        """
+        Defines a setup method that performs a one
+        time operation on program startup.
+        Initializes all class objects that inherits
+        from `BaseModel`.
+        """
+        cls.storage = DBStorage()
+        Base.metadata.create_all(cls.storage.__engine)
+        cls.session = sessionmaker(bind=cls.storage.__engine)
+        cls.Session = cls.session()
+        cls.basemodel = BaseModel(name="Base", duty="parent_class")
+        cls.Session.add(cls.basemodel)
+        cls.amenity = Amenity(name="Free_Wifi")
+        cls.Session.add(cls.amenity)
+        cls.review = Review(text="Nice_place")
+        cls.Session.add(cls.review)
+        cls.city = City(name="Ikeja", state_id=cls.state.id)
+        cls.Session.add(cls.city)
+        cls.state = State(name="Lagos")
+        cls.Session.add(cls.state)
+        cls.user = User(email="joe@alx.com", password="betty",
+                        first_name="Joe", last_name="Navi")
+        cls.Session.add(cls.user)
+        cls.place = Place(city_id=cls.city.id, user_id=cls.user.id,
+                          name="Joe", description="Eko-Hotel")
+        cls.Session.add(cls.place)
+        cls.Session.commit()
+        # cls.storage.save()
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_pep8_DBStorage(self):
-        """Test Pep8"""
+    def tearDownClass(cls):
+        """
+        Defines a teardown method that deletes all
+        created objects, closes open connection to
+        databases at the end of program execution
+        """
+        cls.storage.delete(cls.basemodel)
+        cls.storage.delete(cls.amenity)
+        cls.storage.delete(cls.review)
+        cls.storage.delete(cls.city)
+        cls.storage.delete(cls.state)
+        cls.storage.delete(cls.user)
+        cls.storage.delete(cls.place)
+        cls.storage.save()
+        del cls.basemodel
+        del cls.amenity
+        del cls.review
+        del cls.city
+        del cls.state
+        del cls.user
+        del cls.place
+        cls.storage.close()
+        del cls.storage
+
+    def setUp(self):
+        """Creates a usable instance of a class for test"""
+        self.cls_obj = DBStorage()
+        self.state_obj = DBStorage.all(State)
+        self.all_obj = DBStorage.all()
+
+    def tearDown(self):
+        """Deletes the usable instance of a class per run"""
+        del self.cls_obj
+        del self.state_obj
+        del self.all_obj
+
+    # @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+    def test_pep8(self):
+        """Tests pep8 conformance"""
         style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+        pep = style.check_files(["models/engine/db_storage.py"])
+        self.assertEqual(pep.total_errors, 0, "fix pep8 errors")
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_read_tables(self):
-        """existing tables"""
-        self.query.execute("SHOW TABLES")
-        salida = self.query.fetchall()
-        self.assertEqual(len(salida), 7)
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+    def test_class_documentation(self):
+        """Tests class documentation"""
+        self.assertIsNotNone(DBStorage.__doc__)
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_no_element_user(self):
-        """no elem in users"""
-        self.query.execute("SELECT * FROM users")
-        salida = self.query.fetchall()
-        self.assertEqual(len(salida), 0)
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+    def test_class_method_documentation(self):
+        """Tests class methods documentation"""
+        self.assertIsNotNone(DBStorage.all.__doc__)
+        self.assertIsNotNone(DBStorage.new.__doc__)
+        self.assertIsNotNone(DBStorage.save.__doc__)
+        self.assertIsNotNone(DBStorage.delete.__doc__)
+        self.assertIsNotNone(DBStorage.reload.__doc__)
+        self.assertIsNotNone(DBStorage.close.__doc__)
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_no_element_cities(self):
-        """no elem in cities"""
-        self.query.execute("SELECT * FROM cities")
-        salida = self.query.fetchall()
-        self.assertEqual(len(salida), 0)
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+    def test_class_attributes(self):
+        """Tests class has attributes"""
+        self.assertTrue(hasattr(self.cls_obj, "__init__"))
+        self.assertTrue(hasattr(self.cls_obj, "all"))
+        self.assertTrue(hasattr(self.cls_obj, "new"))
+        self.assertTrue(hasattr(self.cls_obj, "save"))
+        self.assertTrue(hasattr(self.cls_obj, "delete"))
+        self.assertTrue(hasattr(self.cls_obj, "reload"))
+        self.assertTrue(hasattr(self.cls_obj, "close"))
+        self.assertTrue(hasattr(self.cls_obj, "__engine"))
+        self.assertTrue(hasattr(self.cls_obj, "__session"))
 
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db', 'NO DB')
-    def test_add(self):
-        """Test same size between storage() and existing db"""
-        self.query.execute("SELECT * FROM states")
-        salida = self.query.fetchall()
-        self.assertEqual(len(salida), 0)
-        state = State(name="LUISILLO")
-        state.save()
-        self.db.autocommit(True)
-        self.query.execute("SELECT * FROM states")
-        salida = self.query.fetchall()
-        self.assertEqual(len(salida), 1)
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+    def test_init(self):
+        """Tests object created from `DBStorage` is not None"""
+        self.assertIsNotNone(self.cls_obj)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+    def test_all(self):
+        """Tests all method"""
+        self.assertIsNotNone(self.state_obj)
+        self.assertTrue(type(self.state_obj), dict)
+        self.assertEqual(len(self.state_obj), 1)
+        self.assertTrue(type(self.all_obj), dict)
+        self.assertEqual(len(self.cls_obj), 7)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+    def test_class_instance(self):
+        """Tests class object is instance of `DBStorage`"""
+        self.assertTrue(isinstance(type(self.cls_obj)), DBStorage)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+    def test_new(self):
+        """Tests new method"""
+        self.cls_obj.new(self.state_obj)
+        self.assertTrue(self.state_obj in self.cls_obj.new)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+    def test_save_and_delete(self):
+        """Tests save and delete method"""
+        state_obj = State(name="Lagos")
+        DBStorage.new(state_obj)
+        city_obj = City(name="Ikeja")
+        DBStorage.new(city_obj)
+        DBStorage.save()
+        db = MySQLdb.connect(user=getenv("HBNB_MYSQL_USER"),
+                             password=getenv("HBNB_MYSQL_PWD"),
+                             host=getenv("HBNB_MYSQL_HOST"),
+                             db=getenv("HBNB_MYSQL_DB"))
+        query_obj = db.cursor()
+        query_obj.execute("SELECT * FROM states WHERE name = 'Lagos'")
+        result = query_obj.fetchall()
+        self.assertEqual(len(result), 1)
+        query_obj.close()
+        query_obj = db.cursor()
+        query_obj.execute("SELECT * FROM cities WHERE name = 'cities'")
+        result = query_obj.fetchall()
+        self.assertEqual(len(result), 0)
+        query_obj.close()
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+    def test_delete_None(self):
+        """Tests delete method with None"""
+        try:
+            DBStorage.delete(None)
+        except Exception:
+            self.fail
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+    def test_reload(self):
+        """Tests reload method"""
+        DBStorage.reload()
+        self.assertTrue(isinstance(DBStorage.__session, Session))
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "No DB")
+    def test_id_created_at_updated_at(self):
+        """
+        Tests presence of id, created_at and updated_at
+        on instances of child classes
+        """
+        self.assertIn("id", self.basemodel.__dict__)
+        self.assertIn("created_at", self.basemodel.__dict__)
+        self.assertIn("updated_at", self.basemodel.__dict__)
+        self.assertIn("id", self.state.__dict__)
+        self.assertIn("created_at", self.state.__dict__)
+        self.assertIn("updated_at", self.state.__dict__)
+        self.assertIn("id", self.city.__dict__)
+        self.assertIn("created_at", self.city.__dict__)
+        self.assertIn("updated_at", self.city.__dict__)
+        self.assertIn("id", self.review.__dict__)
+        self.assertIn("created_at", self.review.__dict__)
+        self.assertIn("updated_at", self.review.__dict__)
+        self.assertIn("id", self.user.__dict__)
+        self.assertIn("created_at", self.user.__dict__)
+        self.assertIn("updated_at", self.user.__dict__)
+        self.assertIn("id", self.place.__dict__)
+        self.assertIn("created_at", self.place.__dict__)
+        self.assertIn("updated_at", self.place.__dict__)
+        self.assertIn("id", self.amenity.__dict__)
+        self.assertIn("created_at", self.amenity.__dict__)
+        self.assertIn("updated_at", self.amenity.__dict__)
+
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,95 +1,217 @@
 #!/usr/bin/python3
-"""test for file storage"""
-import unittest
-import pep8
-import json
+"""
+The `test_file_storage` module supplies a test class for the `FileStorage`
+class.
+This implements tests for all class attributes and methods, documentation
+tests, and pep8 conformance.
+"""
+
+
 import os
+import pep8
+import unittest
+from os import getenv
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-from models.user import User
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
-from models.place import Place
 from models.review import Review
-from models.engine.file_storage import FileStorage
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
 
 
-class TestFileStorage(unittest.TestCase):
-    '''this will test the FileStorage'''
+@unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage")
+class Test_FileStorage(unittest.TestCase):
+    """
+    Defines a `Test_FileStorage` class that tests
+    all class attributes and methods.
+    """
 
     @classmethod
     def setUpClass(cls):
-        """set up for test"""
-        cls.user = User()
-        cls.user.first_name = "Kev"
-        cls.user.last_name = "Yo"
-        cls.user.email = "1234@yahoo.com"
+        """
+        Defines a setup class that performs a one time
+        operation on program startup
+        """
+        try:
+            os.rename("file.json", "tmp_file.json")
+        except IOError:
+            pass
         cls.storage = FileStorage()
+        cls.base = BaseModel()
+        cls.storage.new(cls.base)
+        cls.t_base = BaseModel()
+        cls.storage.new(cls.t_base)
+        cls.state = State()
+        cls.storage.new(cls.state)
+        cls.city = City()
+        cls.storage.new(cls.city)
+        cls.review = Review()
+        cls.storage.new(cls.review)
+        cls.user = User()
+        cls.storage.new(cls.user)
+        cls.place = Place()
+        cls.storage.new(cls.place)
+        cls.amenity = Amenity()
+        cls.storage.new(cls.amenity)
 
     @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down"""
-        del cls.user
-
-    def tearDown(self):
-        """teardown"""
+    def tearDownClass(cls):
+        """
+        Defines a method that performs a one time operation
+        after execution of all programs
+        """
         try:
             os.remove("file.json")
-        except Exception:
+        except IOError:
             pass
+        try:
+            os.rename("tmp_file.json", "file.json")
+        except IOError:
+            pass
+        del cls.base
+        del cls.t_base
+        del cls.state
+        del cls.city
+        del cls.review
+        del cls.user
+        del cls.place
+        del cls.amenity
 
-    def test_pep8_FileStorage(self):
-        """Tests pep8 style"""
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage")
+    def test_pep8(self):
+        """Tests pep8 conformance"""
         style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+        pep = style.check_files(["models/engine/file_storage.py"])
+        self.assertEqual(pep.total_errors, 0, "fix pep8 error")
 
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage")
+    def test_class_documentation(self):
+        """Tests documentation for `FileStorage` class"""
+        self.assertIsNotNone(FileStorage.__doc__)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage")
+    def test_class_methods_doc(self):
+        """Tests class methods has documentations"""
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
+        self.assertIsNotNone(FileStorage.delete.__doc__)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage")
+    def test_class_attributes_doc(self):
+        """Tests class attributes documentation"""
+        self.assertTrue(hasattr(self.storage, "all"))
+        self.assertTrue(hasattr(self.storage, "new"))
+        self.assertTrue(hasattr(self.storage, "save"))
+        self.assertTrue(hasattr(self.storage, "reload"))
+        self.assertTrue(hasattr(self.storage, "delete"))
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage")
+    def test_isinstance(self):
+        """Tests object is instance of `FileStorage`"""
+        self.assertIsInstance(self.storage, FileStorage)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage")
     def test_all(self):
-        """tests if all works in File Storage"""
-        storage = FileStorage()
-        obj = storage.all()
-        self.assertIsNotNone(obj)
-        self.assertEqual(type(obj), dict)
-        self.assertIs(obj, storage._FileStorage__objects)
+        """Tests all method"""
+        result = self.storage.all(self.state)
+        self.assertTrue(type(result), dict)
+        self.assertIsNotNone(result)
+        self.assertTrue("BaseModel." +
+                        self.base.id in FileStorage._FileStorage__objects)
+        self.assertTrue("State." +
+                        self.state.id in FileStorage._FileStorage__objects)
+        self.assertTrue("City." +
+                        self.city.id in FileStorage._FileStorage__objects)
+        self.assertTrue("Review." +
+                        self.review.id in FileStorage._FileStorage__objects)
+        self.assertTrue("User." +
+                        self.user.id in FileStorage._FileStorage__objects)
+        self.assertTrue("Place." +
+                        self.place.id in FileStorage._FileStorage__objects)
+        self.assertTrue("Amenity." +
+                        self.amenity.id in FileStorage._FileStorage__objects)
 
-    def test_new(self):
-        """test when new is created"""
-        storage = FileStorage()
-        obj = storage.all()
-        user = User()
-        user.id = 123455
-        user.name = "Kevin"
-        storage.new(user)
-        key = user.__class__.__name__ + "." + str(user.id)
-        self.assertIsNotNone(obj[key])
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage")
+    def test_save(self):
+        """Tests save method for presence of instance ids"""
+        self.storage.save()
+        with open("file.json", "r", encoding="utf-8") as file:
+            textFile = file.read()
+        self.assertTrue("BaseModel." + self.base.id in textFile)
+        self.assertTrue("State." + self.state.id in textFile)
+        self.assertTrue("City." + self.city.id in textFile)
+        self.assertTrue("Review." + self.review.id in textFile)
+        self.assertTrue("User." + self.user.id in textFile)
+        self.assertTrue("Place." + self.place.id in textFile)
+        self.assertTrue("Amenity." + self.amenity.id in textFile)
 
-    def test_reload_filestorage(self):
-        """
-        tests reload
-        """
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage")
+    def test_reload(self):
+        """Tests reload method"""
+        self.storage.reload()
+        self.assertTrue("BaseModel." +
+                        self.base.id in FileStorage._FileStorage__objects)
+        self.assertTrue("State." +
+                        self.state.id in FileStorage._FileStorage__objects)
+        self.assertTrue("City." +
+                        self.city.id in FileStorage._FileStorage__objects)
+        self.assertTrue("Review." +
+                        self.review.id in FileStorage._FileStorage__objects)
+        self.assertTrue("User." +
+                        self.user.id in FileStorage._FileStorage__objects)
+        self.assertTrue("Place." +
+                        self.place.id in FileStorage._FileStorage__objects)
+        self.assertTrue("Amenity." +
+                        self.amenity.id in FileStorage._FileStorage__objects)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage")
+    def test_delete(self):
+        """Tests delete method"""
         self.storage.save()
-        Root = os.path.dirname(os.path.abspath("console.py"))
-        path = os.path.join(Root, "file.json")
-        with open(path, 'r') as f:
-            lines = f.readlines()
-        try:
-            os.remove(path)
-        except:
-            pass
+        self.storage.delete(self.t_base)
         self.storage.save()
-        with open(path, 'r') as f:
-            lines2 = f.readlines()
-        self.assertEqual(lines, lines2)
-        try:
-            os.remove(path)
-        except:
-            pass
-        with open(path, "w") as f:
-            f.write("{}")
-        with open(path, "r") as r:
-            for line in r:
-                self.assertEqual(line, "{}")
-        self.assertIs(self.storage.reload(), None)
+        self.assertNotIn(self.t_base, FileStorage._FileStorage__objects)
+        self.assertFalse(self.t_base in FileStorage._FileStorage__objects)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage")
+    def test_created_updated_at(self):
+        """
+        Tests presence created_at, updated_at
+        """
+        self.assertTrue("created_at", self.storage.__dict__)
+        self.assertTrue("updated_at", self.storage.__dict__)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "FileStorage")
+    def test_id_created_at_updated_at(self):
+        """
+        Tests presence of id, created_at and updated_at
+        on instances of child classes
+        """
+        self.assertIn("id", self.base.__dict__)
+        self.assertIn("created_at", self.base.__dict__)
+        self.assertIn("updated_at", self.base.__dict__)
+        self.assertIn("id", self.state.__dict__)
+        self.assertIn("created_at", self.state.__dict__)
+        self.assertIn("updated_at", self.state.__dict__)
+        self.assertIn("id", self.city.__dict__)
+        self.assertIn("created_at", self.city.__dict__)
+        self.assertIn("updated_at", self.city.__dict__)
+        self.assertIn("id", self.review.__dict__)
+        self.assertIn("created_at", self.review.__dict__)
+        self.assertIn("updated_at", self.review.__dict__)
+        self.assertIn("id", self.user.__dict__)
+        self.assertIn("created_at", self.user.__dict__)
+        self.assertIn("updated_at", self.user.__dict__)
+        self.assertIn("id", self.place.__dict__)
+        self.assertIn("created_at", self.place.__dict__)
+        self.assertIn("updated_at", self.place.__dict__)
+        self.assertIn("id", self.amenity.__dict__)
+        self.assertIn("created_at", self.amenity.__dict__)
+        self.assertIn("updated_at", self.amenity.__dict__)
 
 
 if __name__ == "__main__":
