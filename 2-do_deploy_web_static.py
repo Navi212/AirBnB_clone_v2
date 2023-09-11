275e273152761e9bd7181096df10d9b4dbfa177f
Joseph Nweke <114510416+Navi212@users.noreply.github.com>
2023-09-11 02:59:26 +0100

    Update 2-do_deploy_web_static.py
    Removed blank lines

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,31 +13,6 @@ import os
 env.user = "ubuntu"
 env.hosts = ['52.86.24.88', '100.26.20.84']
 
-
-@task
-def do_pack():
-    """
-    Defines a function that generates a .tgz archive from the contents
-    of the web_static folder of AirBnB Clone repo
-    Requirements:
-        All files in the folder web_static must be added to the final archive
-        All archives must be stored in the folder versions (your function
-            should create this folder if it doesn’t exist)
-        The name of the archive created must be
-            web_static_<year><month><day><hour><minute><second>.tgz
-        The function do_pack must return the archive path if the archive has
-            been correctly generated. Otherwise, it should return None
-    """
-   if local("mkdir -p versions").failed:
-        return None
-    time_fm = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = f"web_static_{time_fm}.tgz"
-    dest_folder = f"versions/{file_name}"
-    if local(f"tar -cvzf {dest_folder} web_static").failed:
-        return None
-    return file_name
-
-
 @task
 def do_deploy(archive_path):
     """
@@ -75,13 +50,3 @@ def do_deploy(archive_path):
     if run(f"ln -sf {dir_path}/{file_dir} {symlink}").failed:
         return False
     return True
-
-
-
-
-
-
-
-
-
-
