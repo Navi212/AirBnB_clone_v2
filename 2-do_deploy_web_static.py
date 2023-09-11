7f105f84c2903eb7fc836afe37d16e11ef093b0b
Joseph Nweke <114510416+Navi212@users.noreply.github.com>
2023-09-11 02:07:06 +0100

    Update 2-do_deploy_web_static.py
    Removed bug

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ The `2-do_deploy_web_static` module supplies a function `do_deploy` that creates
 and distributes an archive to our web servers based on `1-pack_web_static` module
 """
 
-
+from __future__ import with_statement
 from fabric.api import *
 from datetime import datetime
 import os
@@ -57,43 +57,15 @@ def do_deploy(archive_path):
             = ['<IP web-01>', 'IP web-02'] variable in your script)
         Returns True if all operations have been done correctly, otherwise False
     """
-    try:
-            if not (path.exists(archive_path)):
-                    return False
-
-            # upload archive
-            put(archive_path, '/tmp/')
-
-            # create target dir
-            timestamp = archive_path[-18:-4]
-            run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
-
-            # uncompress archive and delete .tgz
-            run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-                .format(timestamp, timestamp))
-
-            # remove archive
-            run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-
-            # move contents into host web_static
-            run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-            # remove extraneous web_static dir
-            run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'
-                .format(timestamp))
-
-            # delete pre-existing sym link
-            run('sudo rm -rf /data/web_static/current')
-
-            # re-establish symbolic link
-            run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
-    except:
+    if not path.exists(archive_path):
+        return False
+    with settings(warn_only=True):
+        if put("archive_path", "/tmp/", capture=True).failed:
             return False
-
-    # return True on success
-    return True
+        file_name = f"web_static_{time_fm}"
+        if run(f"tar -xvzf archive_path -C /data/web_static/releases/{file_name}", capture=True).failed:
+            return False
+        run("rm -f /tmp/archive_path")
+        if run("ln -sf /data/web_static/releases/{file_name} /data/web_static/current", capture=True).failed:
+            return False
+        return True
