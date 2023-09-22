bd677e5bdd5f01dabbe623583e0b58b60852e743
Navi212 <josephchristo615@gmail.com>
2023-09-22 15:16:52 +0100

    Adds port and removed p tag

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -13,13 +13,13 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
+def hello_hbnb():
     """
     Defines a function that display “Hello HBNB!”
     when queried at its root.
     """
-    return f"<p>Hello HBNB!</p>"
+    return f"Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host=0.0.0.0)
+    app.run(host=0.0.0.0, port=5000)
