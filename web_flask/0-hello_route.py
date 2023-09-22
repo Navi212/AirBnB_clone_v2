c14876b5e28aafeafb2c05b73e50545877a6a67f
Navi212 <josephchristo615@gmail.com>
2023-09-22 15:36:51 +0100

    Adds quote to enclose ip

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+"""
+The `0-hello_route` supplies a function `hello_hbnb`
+that starts a Flask web application, listens to port
+5000 on any ip address and returns a string when queried
+"""
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """ Function called with / route """
-    return 'Hello HBNB!'
+    """
+    Returns 'Hello HBNB!' when queried at '/'
+    """
+    return f"Hello HBNB!"
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
