c3ad0728a91eb04ac1a09d674d73f4b7d0beddce
Navi212 <josephchristo615@gmail.com>
2023-09-22 15:23:50 +0100

    Removes blank lines

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,8 +4,6 @@ The `0-hello_route` module supplies a function
 that starts a Flask web application and returns a
 text when queried at its root.
 """
-
-
 from flask import Flask
 
 
@@ -18,7 +16,7 @@ def hello_hbnb():
     Defines a function that display “Hello HBNB!”
     when queried at its root.
     """
-    return f"Hello HBNB!"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
