d8df486184e129692d93a84f275a36fcef3615d2
Navi212 <josephchristo615@gmail.com>
2023-09-22 16:32:49 +0100

    Adds string formatter

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -35,7 +35,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """ Function called with /c/<text> route """
-    return 'C %s' % text.replace('_', ' ')
+    return f"C {text.replace('_', ' ')}"
 
 
 if __name__ == "__main__":
