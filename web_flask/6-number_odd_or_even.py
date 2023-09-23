0b418e2cb1d3994ea00cb582ca2804fcd2759d9a
Joseph Nweke <114510416+Navi212@users.noreply.github.com>
2023-09-23 14:51:58 +0100

    Update 6-number_odd_or_even.py
    Updates variable name in render_template fun

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -72,7 +72,7 @@ def render_temp(n):
     """
     Displays a html page only if "n" is an integer
     """
-    return render_template("5-number.html", number=n)
+    return render_template("5-number.html", n=n)
 
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
