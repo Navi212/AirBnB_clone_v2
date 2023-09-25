d7252b690d672be6c37c3ebc1c57d56a3e578818
Navi212 <josephchristo615@gmail.com>
2023-09-25 10:47:06 +0100

    Adds state.py file

--- models/state.py
+++ models/state.py
@@ -18,22 +18,22 @@ class State(BaseModel, Base):
     cities        : relationship to the `City` class or table
     """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",
-                          cascade="all, delete, delete-orphan",
-                          backref="state")
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+    else:
+        name = ''
 
     @property
     def cities(self):
+        """Returns the list of `City` instances
+        with `state_id` equals to the current
         """
-        Returns the list of City instances with state_id
-        equals to the current State.id => This will be the
-        FileStorage relationship between State and City
-        """
-        from models import storage
-        city_li = []
-        city_objs = storage.all(City).values()
-        for obj in city_objs:
-            if obj.state_id == self.id:
-                city_li.append(obj)
-        return (city_li)
+        cities = list()
+
+        for _id, city in models.storage.all(City).items():
+            if city.state_id == self.id:
+                cities.append(city)
+        return cities
